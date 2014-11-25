95fd0a9bf15f3c9fd6baf51c0c85fe5b35e472dc
Matthew McNeilly <mcneilly-m1@emai.ulster.ac.uk>
2014-11-25 00:43:36 +0000

    Styling, Added & Attempted Functionality
    Styling - Books.asp
    Added Functionality - Books.aspx - Price updates based on selected type
    of book - use of functions
    Attempted Functionality - Shopping Cart - Books.aspx

--- Pages/Books.aspx.vb
+++ Pages/Books.aspx.vb
@@ -2,4 +2,42 @@
 Partial Class Page_Books
     Inherits System.Web.UI.Page
 
+    Function findStandardPrice() As Double
+
+        Dim dblStandardPrice As Double = 0
+        Dim intQuantity As Integer = CInt(txtQuantity.Text)
+
+
+        If DropDownList1.SelectedIndex = 0 Then
+            dblStandardPrice = 5.0
+        ElseIf DropDownList1.SelectedIndex = 1 Then
+            dblStandardPrice = 8.0
+        Else
+            dblStandardPrice = 3.0
+        End If
+
+        Return dblStandardPrice
+    End Function
+
+
+
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
+        lblPrice.Text = findStandardPrice()
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Session("intNumItems") += 1
+
+        lblNumItems.Text = Session("intNumItems")
+       
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+
+
+        lblNumItems.Text = Session("intNumItems")
+
+    End Sub
 End Class
